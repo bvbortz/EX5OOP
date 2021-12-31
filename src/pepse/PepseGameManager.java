bc8d581c99f0af041c9d084b8a48be2744c59efa
bvbortz <Barry.Bortz@mail.huji.ac.il>
2021-12-31 12:09:35 +0200

    night step 1 finished

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -7,6 +7,7 @@ import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
 import pepse.world.Sky;
+import pepse.world.daynight.Night;
 
 public class PepseGameManager extends GameManager {
 
@@ -18,5 +19,6 @@ public class PepseGameManager extends GameManager {
     public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
         Sky.create(gameObjects(), windowController.getWindowDimensions(), Layer.BACKGROUND);
+        Night.create(gameObjects(), windowController.getWindowDimensions(), 10f, Layer.FOREGROUND);
     }
 }
--- src/pepse/world/daynight/Night.java
+++ src/pepse/world/daynight/Night.java
@@ -1,4 +1,26 @@
 package pepse.world.daynight;
 
+import danogl.GameObject;
+import danogl.collisions.GameObjectCollection;
+import danogl.components.CoordinateSpace;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.util.Vector2;
+
+import java.awt.*;
+
 public class Night {
+    private static final Color NIGHT_COLOR = Color.BLACK;
+    public static GameObject create(
+            GameObjectCollection gameObjects,
+            Vector2 windowDimensions,
+            float cycleLength,
+            int layer){
+        GameObject night = new GameObject(
+                Vector2.ZERO, windowDimensions,
+                new RectangleRenderable(NIGHT_COLOR));
+        night.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        gameObjects.addGameObject(night, layer);
+        night.setTag("night");
+        return night;
+    }
 }
