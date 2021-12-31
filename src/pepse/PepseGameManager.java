7a6b632514b8fb3f8be994e2136145253b0d3174
bvbortz <Barry.Bortz@mail.huji.ac.il>
2021-12-31 11:34:36 +0200

    Sky done

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -1,6 +1,12 @@
 package pepse;
 
 import danogl.GameManager;
+import danogl.collisions.Layer;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import pepse.world.Sky;
 
 public class PepseGameManager extends GameManager {
 
@@ -8,4 +14,9 @@ public class PepseGameManager extends GameManager {
         new PepseGameManager().run();
     }
 
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        Sky.create(gameObjects(), windowController.getWindowDimensions(), Layer.BACKGROUND);
+    }
 }
